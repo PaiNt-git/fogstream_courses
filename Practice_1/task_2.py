ea1ef4b5302a19c8452aee907c217b6858d00496
ASZabavin <ASZabavin@pnu.edu.ru>
2018-10-16 23:32:36 +1000

    Фикс 2 задачи еще 23-35 :)

--- Practice_1/task_2.py
+++ Practice_1/task_2.py
@@ -16,7 +16,7 @@ def main(v, t):
 
     lemainder = l % MOSCOW_LENGTH
 
-    return int(lemainder) if v >= 0 else MOSCOW_LENGTH - lemainder
+    return int(lemainder) if v >= 0 else MOSCOW_LENGTH - int(lemainder)
 
 
 if __name__ == '__main__':
